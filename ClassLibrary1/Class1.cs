dc378b16022c1b135399dbc57cde201b8f5612e6
Aditi <aditidarade1@gmail.com>
2024-10-21 13:26:35 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface ILoanable
+    {
+        int LoanPeriod { set; get; } //property
+        string Borrower { set; get; } //property
+        int Borrow(System.DateTime BorrowDate); //method
+        int Return(System.DateTime ReturnDate); //method
+    }
+
+    public interface IPrintable
+    {
+        void Print(); //method
+    }
+
+    public class Book : ILoanable, IPrintable
+    {
+        public string author;
+        public string title;
+        public long iSBN;
+        public Book(string Author, string Title, long ISBN)
+        {
+            author = Author;
+            title = Title;
+            iSBN = ISBN;
+        }
+
+        public int loanPeriod;
+        public string borrower;
+        public System.DateTime borrowDate;
+        public System.DateTime returnDate;
+
+        public int LoanPeriod //property
+        {
+            set
+            {
+                if (value <= 21)
+                {
+                    loanPeriod = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Loan Period of a book cannot be over 21 days");
+                    loanPeriod = value;
+                }
+            }
+            get
+            {
+                return loanPeriod;
+                //return 21;
+            }
+        }
+        public string Borrower //property
+        {
+            set
+            {
+                borrower = value;
+            }
+            get
+            {
+                return borrower;
+
+            }
+        }
+        public int Borrow(System.DateTime BorrowDate) //method
+        {
+            borrowDate = BorrowDate;
+            //System.DateTime borrowDate;
+            //Convert.ToInt32(borrowDate);
+            return 0;
+        }
+        public int Return(System.DateTime ReturnDate) //method
+        {            
+            if ((ReturnDate - borrowDate).TotalDays <= 21)
+            {
+                returnDate = ReturnDate;
+            }
+            else
+            {
+                System.Console.WriteLine("You have returned the book after 21 days. You need to pay a penalty fee.");
+                returnDate = ReturnDate;
+            }
+            return 0;
+        }
+
+        public void Print() //method
+        {
+            System.Console.WriteLine("\nAuthor = " + author + ", Title = " + title + ", ISBN = " + iSBN + "\nLoan Period of the book is " + loanPeriod + " days. \nBorrower's name is " + borrower + "\nBorrow Date is " + borrowDate + "\nReturn Date is " + returnDate);
+            //return author;
+        }
+    }
+
+
+    public class DVD : ILoanable, IPrintable
+    {
+        public string director;
+        public string title;
+        public int lengthInMinutes;
+        public DVD(string Director, string Title, int LengthInMinutes)
+        {
+            director = Director;
+            title = Title;
+            lengthInMinutes = LengthInMinutes;
+        }
+
+        public int loanPeriod;
+        public string borrower;
+        public System.DateTime borrowDate;
+        public System.DateTime returnDate;
+
+        public int LoanPeriod //property
+        {
+            set
+            {
+                if (value <= 7)
+                {
+                    loanPeriod = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Loan Period of a DVD cannot be over 7 days");
+                    loanPeriod = value;
+                }
+            }
+            get
+            {
+                return loanPeriod;
+            }
+        }
+        public string Borrower //property
+        {
+            set
+            {
+                borrower = value;
+            }
+            get
+            {
+                return borrower;
+
+            }
+        }
+        public int Borrow(System.DateTime BorrowDate) //method
+        {
+            borrowDate = BorrowDate;
+            //System.DateTime borrowDate;
+            //Convert.ToInt32(borrowDate);
+            return 0;
+        }
+        public int Return(System.DateTime ReturnDate) //method
+        {
+            if ((ReturnDate - borrowDate).TotalDays <= 7)
+            {
+                returnDate = ReturnDate;
+            }
+            else
+            {
+                System.Console.WriteLine("You have returned the DVD after 7 days. You need to pay a penalty fee.");
+                returnDate = ReturnDate;
+            }
+            return 0;
+        }
+
+        public void Print() //method
+        {
+            System.Console.WriteLine("\nDirector = " + director + ", Title = " + title + ", LengthInMinutes = " + lengthInMinutes + "\nLoan Period of the book is " + loanPeriod + " days. \nBorrower's name is " + borrower + "\nBorrow Date is " + borrowDate + "\nReturn Date is " + returnDate);
+            //return author;
+        }
+    }
+
+    public class CD : ILoanable, IPrintable
+    {
+        public string artist;
+        public string title;
+        public int numberOfTracks;
+        public CD(string Artist, string Title, int NumberOfTracks)
+        {
+            artist = Artist;
+            title = Title;
+            numberOfTracks = NumberOfTracks;
+        }
+
+        public int loanPeriod;
+        public string borrower;
+        public System.DateTime borrowDate;
+        public System.DateTime returnDate;
+
+        public int LoanPeriod //property
+        {
+            set
+            {
+                if (value <= 14)
+                {
+                    loanPeriod = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Loan Period of a CD cannot be over 14 days");
+                    loanPeriod = value;
+                }
+            }
+            get
+            {
+                return loanPeriod;
+            }
+        }
+        public string Borrower //property
+        {
+            set
+            {
+                borrower = value;
+            }
+            get
+            {
+                return borrower;
+
+            }
+        }
+        public int Borrow(System.DateTime BorrowDate) //method
+        {
+            borrowDate = BorrowDate;
+            //System.DateTime borrowDate;
+            //Convert.ToInt32(borrowDate);
+            return 0;
+        }
+        public int Return(System.DateTime ReturnDate) //method
+        {
+            if ((ReturnDate - borrowDate).TotalDays <= 14)
+            {
+                returnDate = ReturnDate;
+            }
+            else
+            {
+                System.Console.WriteLine("You have returned the CD after 14 days. You need to pay a penalty fee.");
+                returnDate = ReturnDate;
+            }
+            return 0;
+        }
+
+        public void Print() //method
+        {
+            System.Console.WriteLine("\nArtist = " + artist + ", Title = " + title + ", NumberOfTracks = " + numberOfTracks + "\nLoan Period of the book is " + loanPeriod + " days. \nBorrower's name is " + borrower + "\nBorrow Date is " + borrowDate + "\nReturn Date is " + returnDate);
+            //return author;
+        }
+    }
+
+
+}
---
+++ Sec11_Assign28_LibraryAssign/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ClassLibrary1.Book;
+
+namespace Sec11_Assign28_LibraryAssign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book (Author: "C. S. Lewis", Title: "The Chronicles of Narnia", ISBN: 9780000355812); //string Author, string Title, int ISBN
+            b1.LoanPeriod = 20;
+            b1.Borrower = "Asa";
+            b1.Borrow(System.Convert.ToDateTime("2000-05-25"));
+            b1.Return(DateTime.Now);                
+            b1.Print();
+            System.Console.WriteLine("-------------------------------------------------\n");
+
+            DVD d1 = new DVD("Henry Selick", "Coraline", 115); //string Director, string Title, int LengthInMinutes
+            d1.LoanPeriod = 25;
+            d1.Borrower = "Sam";
+            d1.Borrow(System.Convert.ToDateTime("2024-05-25"));
+            d1.Return(DateTime.Now);
+            d1.Print();
+            System.Console.WriteLine("-------------------------------------------------\n");
+
+            CD c1 = new CD("Gracie Abrams", "minor", 7); //string Artist, string Title, int NumberOfTracks
+            c1.LoanPeriod = 30;
+            c1.Borrower = "Row";
+            c1.Borrow(System.Convert.ToDateTime("2024-10-5"));
+            c1.Return(DateTime.Now);
+            c1.Print();
+            System.Console.WriteLine("-------------------------------------------------\n");
+
+            System.Console.ReadKey();
+        }
+    }
+}
